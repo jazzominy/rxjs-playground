5ae58d019630dbd4ff59c1146f64274fc3a28f6e
jazzominy <jasmine758@gmail.com>
2019-12-29 15:35:46 +0530

    sharing same emitted value

--- app/subject.js
+++ app/subject.js
@@ -2,20 +2,29 @@ import { Observable } from "rxjs";
 
 let observable = Observable.interval(1000).take(5);
 
+let bridgeObs = {
+    next: function(x) {this.observers.forEach(o => o.next(x))},
+    error: function(x) {this.observers.forEach(o => o.error(err))},
+    complete: function(x) {this.observers.forEach(o => o.complete())},
+    observers: [],
+    addObserver: function(o) {this.observers.push(o)}
+}
+
 let obsA = {
     next: (x) => console.log('A next ', x),
     error: (err) => console.log('A error ', err),
-    complete: (x) => console.log('A complete ', x),
+    complete: () => console.log('A complete '),
 }
 
-observable.subscribe(obsA);
+observable.subscribe(bridgeObs);
+bridgeObs.addObserver(obsA);
 
 let obsB = {
     next: (x) => console.log('B next ', x),
     error: (err) => console.log('B error ', err),
-    complete: (x) => console.log('B complete ', x),
+    complete: () => console.log('B complete '),
 }
 
 setTimeout(() => {
-    observable.subscribe(obsB);
+    bridgeObs.addObserver(obsB);
 }, 2000)
